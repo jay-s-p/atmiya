75651c97cae975fa514bbbf71339a87976587ede
jay <jaysparmar303@gmail.com>
2022-06-24 00:13:31 +0530

    🥲 Bug fixed and Table ui complate

--- script.js
+++ script.js
@@ -30,57 +30,89 @@ var timeTable = [
         { startTime: "09:50", endTime: "10:45", subject: "SE (RVV)" }
     ],
 ]
+selectedDay = "Monday";
+window.addEventListener("resize", function (event) {
+    abcd();
+})
 
-
+function abcd()
+{
+    if (this.document.body.clientWidth > 1500) {
+        this.document.getElementById("wed").click();
+        setTimeout(function () {
+            this.document.getElementById("slider").removeAttribute("uk-slider");
+            document.getElementById("uk-extra").style.display = "none";
+        }, 500);
+    } else {
+        this.document.getElementById("slider").setAttribute("uk-slider", 'center: true');
+        document.getElementById("uk-extra").style.display = "block";
+    }
+    console.log("lol");
+}
+setTimeout(function () {
+    abcd();
+}, 300);
 let i = 0;
-// for (let day of timeTable) {
+for (let day of timeTable) {
 
-//     // create li tag
-//     let li = document.createElement("li");
+    // create li tag
+    let li = document.createElement("li");
 
-//     // create table container
-//     let container = document.createElement("div");
-//     container.className = "container"; // adding class name
-//     li.appendChild(container); // append to li
+    // create table container
+    let container = document.createElement("div");
+    container.className = "container center"; // adding class name
+    li.appendChild(container); // append to li
 
-//     // create h1 header
-//     let h1 = document.createElement("h1");
-//     h1.className = "header"; // add class name
-//     h1.innerText = days[i];
-//     container.appendChild(h1); // append h1 to container
+    // create card div
+    let card = document.createElement("div");
+    card.className = "card"; //  add class name
+    container.appendChild(card); // append to container
 
-//     // create table
-//     let table = document.createElement("table");
-//     table.className = "tt"; // give it class name
-//     container.appendChild(table); // append table to table container
+    // create h1 header
+    let h1 = document.createElement("h1");
+    h1.className = "header"; // add class name
+    h1.innerText = days[i];
+    card.appendChild(h1); // append h1 to card
 
-//     // loop for tr
-//     for (data of day) {
-//         // create tr element
-//         let tr = document.createElement("tr");
+    // create table
+    let table = document.createElement("table");
+    table.className = "tt"; // give it class name
+    card.appendChild(table); // append table to table card
 
-//         // TD logic
-//         if(data.subject == "RECESS")
-//         {
-//             let td = document.createElement("td");
-//             td.innerHTML = data.subject;
-//             td.colSpan=2;
-//             tr.appendChild(td);
-//         } else {
-//             let td1 = document.createElement("td");
-//             let td2 = document.createElement("td");
-//             td1.innerText = data.startTime;
-//             td2.innerHTML = data.subject;
-//             tr.appendChild(td1);
-//             tr.appendChild(td2);
-//         }
-//         console.log(data);
-        
-        
-//         table.appendChild(tr); // append tr to table
-//     }
+    // loop for tr
+    let len = day.length;
+    let index = 0;
+    for (data of day) {
+        // create tr element
+        let tr = document.createElement("tr");
 
-//     document.getElementById("container").appendChild(li);
+        // TD logic
+        if (data.subject == "RECESS") {
+            let td = document.createElement("td");
+            td.innerHTML = data.subject;
+            td.colSpan = 2;
+            tr.appendChild(td);
+        } else {
+            let td1 = document.createElement("td");
+            let td2 = document.createElement("td");
+            if (index == 0) {
+                td1.className = "tl";
+                td2.className = "tr";
+            } else if (index == len - 1) {
+                td1.className = "bl";
+                td2.className = "br";
+            }
+            td1.innerText = data.startTime;
+            td2.innerHTML = data.subject;
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+        }
+        index++;
 
-//     i++;
-// }
+        table.appendChild(tr); // append tr to table
+    }
+
+    document.getElementById("list").appendChild(li);
+
+    i++;
+}
