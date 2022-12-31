af2ce67070d7b80f0561dee69a3182325a436e09
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-01-01 01:14:55 +0530

    BY Timetable and Navbar
    OK
    lol
    hmm

--- js/script.js
+++ js/script.js
@@ -14,6 +14,9 @@ const BX2 = "<br />BX2 - ";
 const BX3 = "<br />BX3 - ";
 const SUBJECT = "";
 
+
+
+
 const TIMETABLE_BX = [
     [
         { startTime: "07:30", endTime: "08:25", subject: DOTNET },
@@ -59,14 +62,43 @@ const TIMETABLE_BX = [
 // TODO
 const TIMETABLE_BY = [
     [
-        { startTime: "07:30", endTime: "08:25", subject: SUBJECT },
-        { startTime: "08:25", endTime: "09:20", subject: SUBJECT },
+        { startTime: "07:30", endTime: "08:25", subject: PYTHON },
+        { startTime: "08:25", endTime: "09:20", subject: DOTNET },
         { subject: RECESS },
-        { startTime: "09:50", endTime: "10:45", subject: SUBJECT },
+        { startTime: "09:50", endTime: "10:45", subject: [MINIPROJ, DOTNET, CS] },
+        { subject: RECESS },
+        { startTime: "11:50", endTime: "12:45", subject: TOC },
+    ],
+    [
+        { startTime: "07:30", endTime: "08:25", subject: DOTNET },
+        { startTime: "08:25", endTime: "09:20", subject: TOC },
+        { subject: RECESS },
+        { startTime: "09:50", endTime: "10:45", subject: PYTHON },
+        { startTime: "10:45", endTime: "11:40", subject: EBM },
+        { subject: RECESS },
+        { startTime: "11:50", endTime: "12:45", subject: [PYTHON, MINIPROJ, MCI] },
+    ],
+    [
+        { startTime: "09:50", endTime: "10:45", subject: [DOTNET, PYTHON, MINIPROJ] },
+        { subject: RECESS },
+        { startTime: "11:50", endTime: "12:45", subject: MCI },
+        { startTime: "12:45", endTime: "01:40", subject: TOC },
+    ],
+    [
+        { startTime: "08:25", endTime: "09:20", subject: MCI },
+        { subject: RECESS },
+        { startTime: "09:50", endTime: "10:45", subject: [MCI, CS, DOTNET] },
+        { subject: RECESS },
+        { startTime: "11:50", endTime: "12:45", subject: PYTHON },
+        { startTime: "12:45", endTime: "01:40", subject: EBM },
+    ],
+    [
+        { startTime: "07:30", endTime: "08:25", subject: [CS, MCI, PYTHON] },
+        { subject: RECESS },
+        { startTime: "09:50", endTime: "10:45", subject: MCI },
         { startTime: "10:45", endTime: "11:40", subject: SUBJECT },
         { subject: RECESS },
-        { startTime: "11:50", endTime: "12:45", subject: SUBJECT },
-        { startTime: "12:45", endTime: "01:40", subject: SUBJECT },
+        { startTime: "11:50", endTime: "12:45", subject: QALR },
     ],
 ]
 
@@ -84,7 +116,20 @@ let TIMETABLE = [
     ],
 ]
 
-TIMETABLE = TIMETABLE_BX;
+
+if (!localStorage.getItem("class")) {
+    TIMETABLE = TIMETABLE_BX;
+    localStorage.setItem("class", 0)
+}
+else if (localStorage.getItem("class") == 0) {
+    TIMETABLE = TIMETABLE_BX;
+    document.getElementById("bx").classList.add("active");
+}
+else {
+    document.getElementById("by").classList.add("active");
+    TIMETABLE = TIMETABLE_BY;
+}
+// TIMETABLE = TIMETABLE_BX;
 
 
 var todayDate = (new Date()).getDay(); // getting current date
@@ -111,95 +156,130 @@ setTimeout(function () {
     setProperResponsiveViewForDifferentScreenWidth();
 }, 300);
 
+const setTimeTable = () => {
+    let daysIterator = 0;
+    document.getElementById("list").innerText = "";
+    for (let day of TIMETABLE) {
 
-let daysIterator = 0;
-for (let day of TIMETABLE) {
+        // create li tag
+        let li = document.createElement("li");
 
-    // create li tag
-    let li = document.createElement("li");
+        // create table container
+        let container = document.createElement("div");
+        container.className = "container center"; // adding class name
+        li.appendChild(container); // append to li
 
-    // create table container
-    let container = document.createElement("div");
-    container.className = "container center"; // adding class name
-    li.appendChild(container); // append to li
+        // create card div
+        let card = document.createElement("div");
+        card.className = "card"; //  add class name
+        container.appendChild(card); // append to container
 
-    // create card div
-    let card = document.createElement("div");
-    card.className = "card"; //  add class name
-    container.appendChild(card); // append to container
+        // create h1 header
+        let h1 = document.createElement("h1");
+        h1.className = "header noselect"; // add class name
+        h1.innerText = DAYS[daysIterator];
+        card.appendChild(h1); // append h1 to card
 
-    // create h1 header
-    let h1 = document.createElement("h1");
-    h1.className = "header noselect"; // add class name
-    h1.innerText = DAYS[daysIterator];
-    card.appendChild(h1); // append h1 to card
+        // create table
+        let table = document.createElement("table");
+        table.className = "tt"; // give it class name
+        card.appendChild(table); // append table to table card
 
-    // create table
-    let table = document.createElement("table");
-    table.className = "tt"; // give it class name
-    card.appendChild(table); // append table to table card
+        // loop for tr
+        let index = 0;
+        for (data of day) {
+            let len = day.length;
+            // console.log(data)
+            // console.log(len)
+            // create tr element
+            let tr = document.createElement("tr");
 
-    // loop for tr
-    let index = 0;
-    for (data of day) {
-        let len = day.length;
-        // console.log(data)
-        // console.log(len)
-        // create tr element
-        let tr = document.createElement("tr");
-
-        // TD logic
-        if (data.subject == RECESS) {
-            let td = document.createElement("td");
-            td.className = "noselect";
-            td.innerHTML = RECESS.name;
-            td.colSpan = 2;
-            tr.appendChild(td);
-        } else {
-            let td1 = document.createElement("td");
-            let td2 = document.createElement("td");
-            // console.log(daysIterator + " " + index + " " + len)
-            if (index == 0) {
-                td1.className = "tl noselect";
-                td2.className = "tr noselect";
-                // console.log("top"+daysIterator);
-            } else if (index == len - 1) {
-                // console.log("bottom"+daysIterator);
-                td1.className = "bl noselect";
-                td2.className = "br noselect";
+            // TD logic
+            if (data.subject == RECESS) {
+                let td = document.createElement("td");
+                td.className = "noselect";
+                td.innerHTML = RECESS.name;
+                td.colSpan = 2;
+                tr.appendChild(td);
             } else {
-                td1.className = "noselect";
-                td2.className = "noselect";
+                let td1 = document.createElement("td");
+                let td2 = document.createElement("td");
+                // console.log(daysIterator + " " + index + " " + len)
+                if (index == 0) {
+                    td1.className = "tl noselect";
+                    td2.className = "tr noselect";
+                    // console.log("top"+daysIterator);
+                } else if (index == len - 1) {
+                    // console.log("bottom"+daysIterator);
+                    td1.className = "bl noselect";
+                    td2.className = "br noselect";
+                } else {
+                    td1.className = "noselect";
+                    td2.className = "noselect";
+                }
+                td1.innerText = data.startTime;
+                if (data.subject.length) {
+                    td2.innerHTML =
+                        BX1 + data.subject[0].name +
+                        BX2 + data.subject[1].name +
+                        BX3 + data.subject[2].name;
+                    tr.appendChild(td1);
+                    tr.appendChild(td2);
+                }
+                else {
+                    td2.innerHTML = data.subject.name;
+                    tr.appendChild(td1);
+                    tr.appendChild(td2);
+                }
             }
-            td1.innerText = data.startTime;
-            if (data.subject.length) {
-                td2.innerHTML =
-                    BX1 + data.subject[0].name +
-                    BX2 + data.subject[1].name +
-                    BX3 + data.subject[2].name;
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-            }
-            else {
-                td2.innerHTML = data.subject.name;
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-            }
+
+            table.appendChild(tr); // append tr to table
+
+            ++index;
         }
 
-        table.appendChild(tr); // append tr to table
+        document.getElementById("list").appendChild(li);
 
-        ++index;
+        daysIterator++;
     }
+}
+setTimeTable()
 
-    document.getElementById("list").appendChild(li);
 
-    daysIterator++;
+let setPage = x => {
+    document.getElementById("navbar").checked = false;
+    if (localStorage.getItem("class") == x)
+        return;
+    else if (x == 0) {
+        TIMETABLE = TIMETABLE_BX;
+        document.getElementById("bx").classList.add("active");
+        document.getElementById("by").classList.remove("active");
+    }
+    else{
+        TIMETABLE = TIMETABLE_BY;
+        document.getElementById("bx").classList.remove("active");
+        document.getElementById("by").classList.add("active");
+    }
+    localStorage.setItem("class", x);
+    if (x) {
+        // console.log("by - " + x)
+        document.getElementById("loadingBackground").style.display = "flex";
+        setTimeout(() => {
+            document.getElementById("loadingBackground").style.display = "none";
+        }, 2000)
+    } else {
+        // console.log("bx - " + x)
+        document.getElementById("loadingBackground").style.display = "flex";
+        setTimeout(() => {
+            document.getElementById("loadingBackground").style.display = "none";
+        }, 2000)
+    }
+    setTimeTable();
 }
 
-setTimeout(()=>{
+setTimeout(() => {
     document.getElementById("loadingBackground").style.display = "none";
-},2000)
-setTimeout(()=>{
-    document.querySelector("body > div:nth-child(4)").style.display = "none";
-},1000)
+}, 2000)
+// setTimeout(() => {
+//     document.querySelector("body > div:nth-child(4)").style.display = "none";
+// }, 1000)
